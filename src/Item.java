33010187eb9c864d332bb771c51883b34ce8a006
diepanh287 <121754200+diepanhnguyen2807@users.noreply.github.com>
2024-03-14 07:27:58 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,24 @@
-public class Item {
+public abstract class Item {
+    public String name;
+    
+    private String description;
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    protected String id;
+    
+    default float price;
+    
+    public abstract void showInfo();
+    
+    public Item(String name, String description, String id, float price) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+        this.price = price;
+    }
 }
