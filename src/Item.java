42a44993a15ee13f392638f37e926ad4c02c4922
diepanh287 <121754200+diepanhnguyen2807@users.noreply.github.com>
2024-03-14 08:13:25 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,24 +1,20 @@
-public abstract class Item {
+abstract class Item{
     public String name;
-    
     private String description;
-    public String getDescription() {
+
+    public String getDescription(){
         return description;
     }
-    public void setDescription(String description) {
+    public void setDescription(String description){
         this.description = description;
     }
-    
-    protected String id;
-    
-    default float price;
-    
-    public abstract void showInfo();
-    
-    public Item(String name, String description, String id, float price) {
+    protected String ID;
+    int gia;
+    public abstract void showinfo();
+    public Item(String name, String description, String ID, int gia){
         this.name = name;
         this.description = description;
-        this.id = id;
-        this.price = price;
+        this.ID = ID;
+        this.gia = gia;
     }
 }
