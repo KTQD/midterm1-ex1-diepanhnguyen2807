4c8eeec5aa46dcd0f067f88b1bace6fa4ba09ffa
diepanh287 <121754200+diepanhnguyen2807@users.noreply.github.com>
2024-03-14 08:12:43 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
+import java.awt.print.Book;
+
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3;
-        mp3.showInfo();
-
-        Book book;
-        book.showInfo();
+        MP3 mp3 = new MP3("Lucky", "a song", "1", 28, 7);
+        Book book = new Book("Gone with the wind", "a book", "2", 300000,"Margaret Mitchell",1000,"Novel");
+        book.showinfo();
+        mp3.showinfo();
 
     }
 }
